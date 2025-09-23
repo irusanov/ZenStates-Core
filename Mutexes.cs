18fe7df16ac76e536590a2c4137f54f5c1602d0f
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2025-09-23 18:19:46 +0300

    Extract mutexes

---
+++ Mutexes.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Threading;
+
+namespace ZenStates.Core
+{
+    public static class Mutexes
+    {
+        private static Mutex isaBusMutex;
+        private static Mutex pciBusMutex;
+
+        private static readonly string isaMutexName = "Global\\Access_ISABUS.HTP.Method";
+        private static readonly string pciMutexName = "Global\\Access_PCI";
+
+        static Mutex CreateOrOpenExistingMutex(string name)
+        {
+            try
+            {
+                return new Mutex(false, name);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                try
+                {
+#if NETCOREAPP || NETSTANDARD || NET6_0_OR_GREATER
+                    return MutexAcl.OpenExisting(name, MutexRights.Synchronize);
+#else
+                    return Mutex.OpenExisting(name, MutexRights.Synchronize);
+#endif
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            return null;
+        }
+
+        public static void Open()
+        {
+            isaBusMutex = CreateOrOpenExistingMutex(isaMutexName);
+            pciBusMutex = CreateOrOpenExistingMutex(pciMutexName);
+        }
+
+        public static void Close()
+        {
+            isaBusMutex?.Close();
+            pciBusMutex?.Close();
+        }
+
+        public static bool WaitIsaBus(int millisecondsTimeout)
+        {
+            return isaBusMutex.WaitOne(millisecondsTimeout);
+        }
+
+        public static void ReleaseIsaBus()
+        {
+            isaBusMutex?.ReleaseMutex();
+        }
+
+        public static bool WaitPciBus(int millisecondsTimeout)
+        {
+            return pciBusMutex.WaitOne(millisecondsTimeout);
+        }
+
+        public static void ReleasePciBus()
+        {
+            pciBusMutex?.ReleaseMutex();
+        }
+
+        public static bool WaitMutex(Mutex mutex, int millisecondsTimeout = 5000)
+        {
+            if (pciBusMutex == null)
+                return true;
+
+            try
+            {
+                return pciBusMutex.WaitOne(millisecondsTimeout, false);
+            }
+            catch (AbandonedMutexException)
+            {
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+    }
+}
