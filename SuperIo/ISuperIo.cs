e681a66a693c1c013b736115b976a35fa7da8d82
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2026-08-05 00:45:09 +0300

    superio: add SuperIO definitions

---
+++ SuperIo/ISuperIo.cs
@@ -0,0 +1,26 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Adapted from LibreHardwareMonitor (https://github.com/LibreHardwareMonitor/LibreHardwareMonitor)
+// Partial Copyright (C) Michael Möller <mmoeller@openhardwaremonitor.org> and Contributors.
+
+namespace ZenStates.Core.SuperIo
+{
+    public interface ISuperIo
+    {
+        Chip Chip { get; }
+
+        float?[] Voltages     { get; }
+        float?[] Temperatures { get; }
+        float?[] Fans         { get; }
+        float?[] Controls     { get; }
+
+        void SetControl(int index, byte? value);
+
+        byte? ReadGpio(int index);
+        void  WriteGpio(int index, byte value);
+
+        string GetReport();
+        void   Update();
+        void   Close();
+    }
+}
